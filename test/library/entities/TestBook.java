f07b1496bbb111f3ad10fb74462ae074e09a2140
tiztrain <adamtolo20@hotmail.com>
2019-09-23 22:51:53 +1000

    Added testBorrowFromLibrary method - not correctly setup

--- test/library/entities/TestBook.java
+++ test/library/entities/TestBook.java
@@ -3,15 +3,24 @@ package library.entities;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import library.entities.IBook.BookState;
+
 class TestBook {
 
 	Book book;
+	BookState state;
+	
+	String author;
+	String title;
+	String callNo;
+	int id;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -23,6 +32,7 @@ class TestBook {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		book = new Book(author, title, callNo, id);
 	}
 
 	@AfterEach
@@ -36,6 +46,15 @@ class TestBook {
 		//act
 		//assert
 		assertTrue("The book is available", book.isAvailable());
+	}
+	
+	@Test
+	void testBorrowFromLibrary() {
+		//arrange
+		state = BookState.AVAILABLE;
+		//act
+		//Assert.ThrowsException<System.ArgumentException(()-> )>
+		//assert
 	}
 
 }
