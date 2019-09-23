c0f93d272ed5c7b573d95052c510a4dd1b985d84
tiztrain <adamtolo20@hotmail.com>
2019-09-23 21:53:16 +1000

    Added TestBook.java

---
+++ test/library/entities/TestBook.java
@@ -0,0 +1,41 @@
+package library.entities;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestBook {
+
+	Book book;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		book = null;
+	}
+
+	@Test
+	void testIsAvailable() {
+		//arrange
+		//act
+		//assert
+		assertTrue("The book is available", book.isAvailable());
+	}
+
+}
