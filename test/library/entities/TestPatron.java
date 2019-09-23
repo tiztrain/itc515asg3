91e47c4bba8aabe1b7346e52435c2b8bd63f1156
tiztrain <adamtolo20@hotmail.com>
2019-09-23 21:39:02 +1000

    Submitting TestPatron.java how I think it should be done

--- test/library/entities/TestPatron.java
+++ test/library/entities/TestPatron.java
@@ -4,12 +4,16 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import library.entities.helpers.IBookHelper;
@@ -20,12 +24,16 @@ import library.entities.helpers.IPatronHelper;
 public class TestPatron {
 
 	Patron patron;
+	@Mock Loan loan;
 
 	String lastName = "Smith";
 	String firstName = "John";
 	String email = "john.smith@hotmail.com";
 	long phoneNo = 04123456L;
 	int id = 1;
+	
+	Date currentDate;
+	SimpleDateFormat format;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -38,13 +46,16 @@ public class TestPatron {
 	}
 
 	@Test
-	void testHasOverDueLoans() {
+	void testHasOverDueLoans() throws Exception {
 		//arrange
-		patron.hasOverDueLoans();
+		format = new SimpleDateFormat("dd-MM-yyyy");
+		currentDate = format.parse("01-01-2001");
+		
 		//act
-		patron.restrictBorrowing();
-		//assert
+		loan.checkOverDue(currentDate);
 
+		//assert
+		assertTrue("Loan is overdue", loan.isOverDue());
 	}
 
 }
