67eef681a4a23161a406b6bfd871c1a5158832a9
tiztrain <adamtolo20@hotmail.com>
2019-09-24 22:34:08 +1000

    in testbook class updated testBorrowFromLibraryWhenAVAILABLE & testBorrowFromLibraryWhenONLOAN methods

--- test/library/entities/TestBook.java
+++ test/library/entities/TestBook.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import library.entities.IBook.BookState;
 
@@ -17,10 +18,10 @@ class TestBook {
 	Book book;
 	BookState state;
 	
-	String author;
-	String title;
-	String callNo;
-	int id;
+	String author = "Tolken";
+	String title = "Lord of the Rings";
+	String callNo = "what is this";
+	int id = 1;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -49,13 +50,23 @@ class TestBook {
 	}
 	
 	@Test
-	void testBorrowFromLibrary() {
+	void testBorrowFromLibraryWhenAVAILABLE() {
 		//arrange
-		state = BookState.AVAILABLE;
 		//act		
+		book.borrowFromLibrary();
 		//assert
-		assertThrows(RuntimeException.class, () -> assertEquals(BookState.AVAILABLE, state), (String.format("Book: cannot borrow while book is in state: %s", state)));		
-		state = BookState.ON_LOAN;
+		assertTrue(book.isOnLoan());
 	}
 
+	@Test
+	void testBorrowFromLibraryWhenONLOAN() {
+		//arrange
+		book.borrowFromLibrary();
+		assertTrue(book.isOnLoan());
+		//act
+		Executable e = () -> book.borrowFromLibrary();
+		Throwable t = assertThrows(RuntimeException.class,e);
+		//assert
+		assertEquals("Book: cannot borrow while state does not equal available", t.getMessage());
+	}
 }
